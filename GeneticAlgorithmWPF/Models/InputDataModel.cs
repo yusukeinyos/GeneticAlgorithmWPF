a0e0971849ca0a51bb622395dc7eb3a45007cb66
yusukeinyos <aero.n.404,smith@gmail.com>
2018-03-30 01:31:24 +0900

    add data

--- GeneticAlgorithmWPF/Models/InputDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeneticAlgorithmWPF.Models
-{
-    public class InputDataModel
-    {
-        public int Id { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
-}
